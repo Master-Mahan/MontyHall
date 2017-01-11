5faebd081d655f7d39d997504d92a235a302f707
Victor Valencia <vvalencia@HEQ.CORP.HE>
2017-01-11 15:53:18 -0700

    Creating Initial Monty Hall Commit

---
+++ GameTest/GameTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MontyHall;
+
+namespace GameTest
+{
+    [TestClass]
+    public class GameTest
+    {
+
+
+        [TestMethod]
+        public void ChooseCup()
+        {
+            //ARRANGE
+            Game.Cups middle = Game.Cups.Middle;
+            Game game = new Game(1,middle);
+
+            //ACT
+
+
+            //ASSERT
+            Assert.AreEqual(middle, game.SelectedCup);
+        }
+
+
+        [TestMethod]
+        public void GameExecution()
+        {
+            //Arrange
+            Game game1 = new Game(1,Game.Cups.Left);
+            game1.RemoveCup();
+            bool didUserChange = true;
+            game1.ReassignCup(didUserChange);
+            bool result = game1.Result();
+
+            //Assert
+            
+        }
+
+    }
+}
---
+++ MontyHall/Game.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontyHall
+{
+    public class Game
+    {
+
+
+        public enum Cups
+        {
+            Left = 1,
+            Middle = 2,
+            Right = 3
+        };
+
+
+        private List<Cups> GameCups;
+        public Game(int gameNumber, Cups cup) //We do not want to start a game without first choosing a cup
+        {
+            GameNumber = gameNumber;
+            SelectedCup = cup;
+            PrizedCup = (Cups)new Random().Next(1, 3);
+            InitializeCups();
+       
+        }
+
+        private void InitializeCups()
+        {
+            
+            GameCups = new List<Cups>();
+            GameCups.Add(Cups.Left);
+            GameCups.Add(Cups.Middle);
+            GameCups.Add(Cups.Right);
+        }
+
+        private int GameNumber;
+        public Cups SelectedCup { get; private set; }
+
+
+        //METHODS---------------------------------------------------------------------------------
+        public Cups RemoveCup()
+        {
+            return Cups.Middle;
+        }
+
+        public bool Result()
+        {
+            return true;
+        }
+
+        public void ReassignCup(bool didUserChange)
+        {
+
+        }
+
+        private readonly Cups PrizedCup;
+
+
+        private void FindLeftOverCups()
+        {
+
+        }
+
+
+
+
+    }
+
+
+}
+
